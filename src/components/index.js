c06d099344e4731e83e9835b0b82bdd59ee13297
Anup451 <anupjujagar121@gmail.com>
2023-12-05 16:57:24 +0530

    aded

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar.js";
+import Billing from "./Billing.js";
+import CardDeal from "./CardDeal.js";
+import Business from "./Business.js";
+import Clients from "./Clients.js";
+import CTA from "./CTA.js";
+import Stats from "./Stats.js";
+import Footer from "./Footer.js";
+import Testimonials from "./Testimonials.js";
+import Hero from "./Hero.js";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
